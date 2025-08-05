73b60035bf3f8dc297de0989364b9c9c273c3b7b
Mehmet <@mortvivant>
2025-08-05 21:52:35 +0300

    updated the wrong component of CreateMainCourseRequest

--- src/main/java/com/example/menumaker/business/requests/CreateMainCourseRequest.java
+++ src/main/java/com/example/menumaker/business/requests/CreateMainCourseRequest.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateMainCourseRequest {
+
     private String name;
-    private int id;
+    private int calories;
 }
